6a7bc8c335765ba06e8feb59e21b0853aaada028
andro-jr <prabin20panta@gmail.com>
2024-12-31 15:54:19 +0545

    add: first test case

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,9 @@
+import { toUppercase } from "../app/Utils";
+
+describe("Utils test suite", () => {
+  test("should return uppercase", () => {
+    const result = toUppercase("abc");
+
+    expect(result).toBe("ABC");
+  });
+});
