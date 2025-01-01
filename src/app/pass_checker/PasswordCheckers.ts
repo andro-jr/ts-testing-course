6a0887f889078f87fa053993ec4dcd1e06b6a39f
andro-jr <prabin20panta@gmail.com>
2025-01-01 17:34:16 +0545

    update: test return values updated

--- src/app/pass_checker/PasswordCheckers.ts
+++ src/app/pass_checker/PasswordCheckers.ts
@@ -1,14 +1,36 @@
+enum PasswordErrors {
+  SHORT = "Password should be at-least 8 characters!",
+  NO_UPPER_CASE = "Password must have at-least one uppercase letter",
+  NO_LOWER_CASE = "Password must have at-least one lowercase letter",
+}
+interface CheckResult {
+  isValid: boolean;
+  reasons: PasswordErrors[];
+}
+
 export class PasswordChecker {
-  public checkPassword(password: string): boolean {
+  public checkPassword(password: string): CheckResult {
     if (password.length < 8) {
-      return false;
+      return {
+        isValid: false,
+        reasons: [PasswordErrors.SHORT],
+      };
     }
     if (!/[A-Z]/.test(password)) {
-      return false;
+      return {
+        isValid: false,
+        reasons: [PasswordErrors.NO_UPPER_CASE],
+      };
     }
     if (!/[a-z]/.test(password)) {
-      return false;
+      return {
+        isValid: false,
+        reasons: [PasswordErrors.NO_LOWER_CASE],
+      };
     }
-    return true;
+    return {
+      isValid: true,
+      reasons: [],
+    };
   }
 }
