b87803171c54860f158297107d20b8e264317b95
andro-jr <prabin20panta@gmail.com>
2025-01-01 12:53:34 +0545

    add: string info function to test various matchers

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUppercase } from "../app/Utils";
+import { getStringInfo, toUppercase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("should return uppercase of a valid string", () => {
@@ -11,5 +11,32 @@ describe("Utils test suite", () => {
 
     // assert
     expect(actual).toBe("ABC");
+  });
+
+  it.only("should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+    expect(actual.characters).toHaveLength(9);
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["M", "y", "S", "t", "r", "i", "n", "g"])
+    );
+    expect(actual.characters).toContain<string>("M");
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
